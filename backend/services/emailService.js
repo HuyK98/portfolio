f956db220bd6de0148e4a69a86143c72214e4378
HuyK98 <huytruongdinh61@gmail.com>
2025-10-06 16:48:07 +0700

    fix contact form

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -4,12 +4,15 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 5000,
+    socketTimeout: 10000
 });
 
 transporter.verify(function(error, success) {
